63d192b6d5e6ebdb184d0d4882153c24cde6f777
Muhamad Kosasih <muhamadkosasih28oct@gmail.com>
2023-02-01 19:25:41 +0700

    tempalte

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\DashboardController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::resource('/admin', AdminController::class);
+Route::get('/dashboard', [DashboardController::class, 'dasboardAdmin'])->name('/dashboard');
+
